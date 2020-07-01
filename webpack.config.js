b61cc10b6aea7c5defeac18825ed75ebffe2eb83
Ivan Ruby <ivanrubyds@gmail.com>
2020-07-01 00:39:04 -0400

    Awaiting timeout from multiple failed publish attempts to npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/transaction.js',
+  entry: "./src/transaction.js",
   output: {
-    filename: 'mpesa.mz.nodejs.lib.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
