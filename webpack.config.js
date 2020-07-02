bcaf84a4bf778e8cacc8c1cd107fe41fa7100730
Ivan Ruby <ivanrubyds@gmail.com>
2020-07-02 01:38:23 -0400

    including dist file in root directory

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: "./src/transaction.js",
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, ""),
   },
 };
