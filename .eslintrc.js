2f8a43216736f03658f9709ecd3f63114e52c379
ivanruby <ivanrubyds@gmail.com>
2020-08-23 02:13:48 -0400

    Improving testing and documentation, adding CI

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2020': true
+	},
+	'extends': ['.eslint', 'prettier'],
+	'parserOptions': {
+		'ecmaVersion': 11,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
